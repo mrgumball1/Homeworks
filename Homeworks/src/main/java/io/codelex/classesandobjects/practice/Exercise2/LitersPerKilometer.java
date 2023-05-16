00be7e9f56ffc12968854c9c2c733c46f77ac333
Jevgenijs <ostaninzena@gmail.com>
2023-05-16 15:19:20 +0300

    hotfix delete comments

--- Homeworks/src/main/java/io/codelex/classesandobjects/practice/Exercise2/LitersPerKilometer.java
+++ Homeworks/src/main/java/io/codelex/classesandobjects/practice/Exercise2/LitersPerKilometer.java
@@ -6,8 +6,9 @@ class LitersPerKilometer {
     public static void main( String[] args )  {
         Scanner scan = new Scanner(System.in);
 
-        double startKilometers, endKilometers, liters;
-
+        double startKilometers;
+        double endKilometers;
+        double liters;
         System.out.print("Enter first reading: " );
         startKilometers = scan.nextDouble();
 
@@ -16,13 +17,6 @@ class LitersPerKilometer {
 
         System.out.print("Enter liters consumed: " );
         liters  = scan.nextDouble();
-
-//       // Car car = new Car(startKilometers, endKilometers, liters);
-//
-//        //Car car2 = new Car(100, 235, 50);
-//
-//        System.out.println( "Kilometers per liter are "  + car.calculateConsumption());
-//        System.out.println( "Kilometers per liter are "  + car2.calculateConsumption());
 
     }
 }
