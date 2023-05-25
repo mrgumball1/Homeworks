cfa75234aa3a9a397ed7a00537c612d2a0cac50d
Jevgenijs <ostaninzena@gmail.com>
2023-05-25 12:47:06 +0300

    Exceptions almoust Done

--- Homeworks/src/main/java/io/codelex/exceptions/practice/Exercise1.java
+++ Homeworks/src/main/java/io/codelex/exceptions/practice/Exercise1.java
@@ -1,17 +1,27 @@
 package io.codelex.exceptions.practice;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-
         while (true) {
-            System.out.println("Enter the numerator");
-            int numerator = scanner.nextInt();
-            System.out.println("Enter the divisor");
-            int divisor = scanner.nextInt();
-            System.out.println(numerator + " / " + divisor + " = " + numerator / divisor);
+            try {
+                System.out.println("Enter the numerator or enter q to quit");
+                int numerator = Integer.parseInt(scanner.next());
+                System.out.println("Enter the divisor or enter q to quit");
+                int divisor = Integer.parseInt(scanner.next());
+                System.out.println(numerator + " / " + divisor + " = " + numerator / divisor);
+            } catch (ArithmeticException arithmeticException) {
+                System.out.println("Cant divide by 0 ");
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println("INPUT NUMBERS");
+                if (scanner.next().matches("[q]")) {
+                    break;
+                }
+
+            }
         }
     }
 }
--- Homeworks/src/main/java/io/codelex/exceptions/practice/Exercise4.java
+++ Homeworks/src/main/java/io/codelex/exceptions/practice/Exercise4.java
@@ -1,5 +1,13 @@
 package io.codelex.exceptions.practice;
 
+import java.util.Scanner;
+
+class NonPositiveNumberException extends Exception {
+    public NonPositiveNumberException(String message) {
+        super(message);
+    }
+}
+
 public class Exercise4 {
     public static void main(String[] args) {
         try {
@@ -11,16 +19,17 @@ public class Exercise4 {
             System.out.println("Be sure to enter a number.");
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Enter number as first parameter.");
-        } /* todo -
-         catch (NonPositiveNumberException e) {
-        System.out.println("Result will be imaginary number.");
+        } catch (NonPositiveNumberException e) {
+            System.out.println("Result will be imaginary number.");
         }
-        */
+
     }
 
-    static double getInput(String s) {
-        double d = new Double(s).doubleValue();
-        // Throw an NonPositiveNumberException if d is less than 0
-        return d;
+    static double getInput(String s) throws NonPositiveNumberException {
+        double d = Double.parseDouble(s);
+        if (d < 0) {
+            throw new NonPositiveNumberException("Input positive numbers");
+        }
+            return d;
     }
 }
